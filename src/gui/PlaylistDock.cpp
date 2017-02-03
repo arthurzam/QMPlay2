03d0ad359d182d2b9a4d4af7557ac7e208124c50
Zamarin Arthur <arthurzam@gmail.com>
2017-02-03 07:58:53 +0200

    Enable moving tabs

--- src/gui/PlaylistDock.cpp
+++ src/gui/PlaylistDock.cpp
@@ -60,6 +60,7 @@ PlaylistDock::PlaylistDock() :
 
     m_playlistsW = new QTabWidget;
     m_playlistsW->setTabsClosable(true);
+    m_playlistsW->setMovable(true);
 
     QGridLayout *layout = new QGridLayout(&mainW);
     layout->addWidget(m_playlistsW);
